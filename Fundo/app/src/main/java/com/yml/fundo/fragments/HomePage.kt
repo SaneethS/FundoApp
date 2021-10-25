bac78900c2f6cc62fd914d7b30e1604adca6dd49
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-10-26 00:49:33 +0530

    Updated with toolbar in homepage

--- Fundo/app/src/main/java/com/yml/fundo/fragments/HomePage.kt
+++ Fundo/app/src/main/java/com/yml/fundo/fragments/HomePage.kt
@@ -1,7 +1,12 @@
 package com.yml.fundo.fragments
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.yml.fundo.R
@@ -13,13 +18,14 @@ import com.yml.fundo.viewmodel.SharedViewModelFactory
 class HomePage:Fragment(R.layout.home_page) {
     lateinit var binding: HomePageBinding
     lateinit var sharedViewModel: SharedViewModel
-
+    
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = HomePageBinding.bind(view)
         sharedViewModel = ViewModelProvider(requireActivity(), SharedViewModelFactory())[SharedViewModel::class.java]
+        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.homePageToolbar)
+        setHasOptionsMenu(true)
 
-        var name = arguments?.get("name").toString()
 
         binding.welcomeText.text = "Welcome !!"
 
@@ -27,5 +33,24 @@ class HomePage:Fragment(R.layout.home_page) {
             Authentication.logOut()
             sharedViewModel.setGoToLoginPageStatus(true)
         }
+
+        binding.homePageToolbar.setNavigationOnClickListener {
+            Toast.makeText(requireContext(),"Navigation button clicked",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.toolbar_menu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var itemView = item.itemId
+
+        when(itemView){
+            R.id.profile_icon -> Toast.makeText(requireContext(),"Profile menu clicked",Toast.LENGTH_LONG).show()
+        }
+        return false
     }
 }
+
